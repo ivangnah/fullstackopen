3887db4f0f8f4354d6e11017514197e2ff60cba8
EC2 Default User <ec2-user@ip-172-31-29-29.ap-southeast-1.compute.internal>
2022-01-02 06:20:56 +0000

    completed parts 2.4

--- submissions/part2/src/App.js
+++ submissions/part2/src/App.js
@@ -25,11 +25,8 @@ const Content = (props) => {
 const Sum = (props) => {
   console.log("sum props", props)
   const total = props.parts.reduce((sum, parts) => {
-    console.log("what is happening", sum)
     return sum + parts.exercises
   }, 0)
-
-
   return (
     <div>total of {total} exercises</div>
   )
@@ -46,31 +43,70 @@ const Course = (props) => {
   )
 }
 
+const Catalog = (props) => {
+  console.log("Catalog props", props)
+  console.log("Catalog props parts", props.courses)
+  return (
+    <div>
+    {props.courses.map(courses =>
+      <Course course={courses} />
+    )}
+    
+    </div>
+  )
+}
+
 
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [{
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [{
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [{
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [{
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+        <Header name="Web development curriculum" />
+        <Catalog courses={courses} />
+      </div>
+  )
 }
 
 export default App
