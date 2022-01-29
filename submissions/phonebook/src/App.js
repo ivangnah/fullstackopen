37c8582a44d3efbb2baf19c09f5836d7079264f2
EC2 Default User <ec2-user@ip-172-31-29-29.ap-southeast-1.compute.internal>
2022-01-29 07:29:05 +0000

    added search, only works for perfect match now

--- submissions/phonebook/src/App.js
+++ submissions/phonebook/src/App.js
@@ -13,11 +13,24 @@ const Person  = (props) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number:'999' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+  
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [showAll, setShowAll ] = useState(true)
+  const [searchTerm, setSearchTerm ] = useState('')
 
+  
+  //search works now for perfect match. next thing to do is partial match
+  const personsToShow = showAll
+    ? persons
+    : persons.filter(person => person.name === searchTerm )
+  
+  
   const addName = (event) => {
     event.preventDefault()
     console.log('add button clicked', event.target)
@@ -46,41 +59,66 @@ const App = () => {
     console.log('persons list', persons)
   }
 
+  const showAllNames = (event) => {
+    event.preventDefault()
+    console.log('reset button clicked', event.target)
+    setShowAll(true)
+  }
+
+  //event handler for name change
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  //event handler for number change
   const handleNumberChange = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+  
+  //event handler for search term change
+  const handleSearchTermChange = (event) => {
+    console.log('search term', event.target.value)
+    setSearchTerm(event.target.value)
+    setShowAll(false)
+    console.log('search term 2', searchTerm)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      Filter shown with 
+      <input
+        value={searchTerm}
+        onChange = {handleSearchTermChange}
+        />
       <form onSubmit={addName} >
         <div>
-          name: 
+          <h2> Add a new Name</h2> 
+          Name: 
             <input 
               value={newName} 
               onChange={handleNameChange}
               />
        </div>
        <div>
-        number: 
-            <input 
-              value={newNumber} 
-              onChange={handleNumberChange}
-              />
-        </div>
+          Phone Number: 
+              <input 
+                value={newNumber} 
+                onChange={handleNumberChange}
+                />
+          </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <form onSubmit = {setShowAll}>
+        <button type="submit">Show All</button>
+        </form>
         <div>
-          {persons.map(persons => 
+          {personsToShow.map(persons => 
             <Person key={persons.id} person={persons} />
           )}
       </div>
